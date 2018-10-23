c5cffa2f1335cc0fb511f5c87762d7ee3f746137
JaquelinaAcosta <jaque-acosta@hotmail.com>
2018-10-23 16:36:06 -0300

    Avance en tareas 2 y 3 emitir licencia. historia 1

---
+++ src/CapaLogica/UIEmitirLicenciaLogica.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CapaLogica;
+
+import CapaPresentacion.UIEmitirLicencia;
+import datechooser.beans.DateChooserCombo;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author jaque
+ */
+public class UIEmitirLicenciaLogica {
+    private UIEmitirLicencia emitirLicenciaUI;
+    private JComboBox cbListaCategoria;
+    private JTextField tfObservacion;
+    private JLabel lblFechaVencimiento;
+    private JTextField tfNombreEmpleado;
+    private JTextField ftApellidoEmpleado;
+    private DateChooserCombo dccFechaEmision;
+    private JComboBox cbListaLicencia;
+    
+    public UIEmitirLicenciaLogica (){
+        
+        
+    }
+    
+}
+
+
