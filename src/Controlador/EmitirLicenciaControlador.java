5882018cdc986a41e0bd14dcad2e9cb412a072e0
JaquelinaAcosta <jaque-acosta@hotmail.com>
2018-10-24 15:33:58 -0300

    Avance en tareas 2 y 3 emitir licencia. historia 1

---
+++ src/Controlador/EmitirLicenciaControlador.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.EmitirLicenciaModelo;
+import Vista.EmitirLicenciaVista;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ *
+ * @author jaque
+ */
+public class EmitirLicenciaControlador implements ActionListener {
+    private EmitirLicenciaModelo emitirLicenciaModelo;
+    private EmitirLicenciaVista emitirLicenciaVista;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String comando = e.getActionCommand();
+        switch(comando){
+            case "ACEPTAR":
+                //implementar
+                break;
+            case "CANCELAR":
+                this.emitirLicenciaVista.setVisible(false);
+        }
+    }
+    
+    public EmitirLicenciaControlador(EmitirLicenciaModelo modelo, EmitirLicenciaVista vista){
+        this.emitirLicenciaModelo=emitirLicenciaModelo;
+        this.emitirLicenciaVista=emitirLicenciaVista;
+    }
+    
+    public void iniciar(){
+        emitirLicenciaVista.setTitle("EMITIR LICENCIA");
+        emitirLicenciaVista.setLocationRelativeTo(null);
+    }
+}
+
+
