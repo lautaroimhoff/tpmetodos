d11224a7a45f7fc8794870a3db7c89a1cc1a8048
martindestefanis <tinchodestefanis1@gmail.com>
2018-12-06 11:26:10 -0300

    Filtrado licencias vencidas por fecha TERMINADA

--- test/Calculos/CalcularCostoTest.java
+++ test/Calculos/CalcularCostoTest.java
@@ -33,16 +33,25 @@ public class CalcularCostoTest {
     @Test
     public void testCalcularCosto() {
         System.out.println("CalcularCosto");
-        int CostoAdministrativo = 8;
-        String claseLicencia = "A";
-        int vigencia = 1;
-        int expResult = 28;
-        int result = CalcularCosto.CalcularCosto(claseLicencia, vigencia);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-      
+         //Pruebas verdadera
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("A", 5)==48);
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("B", 4)==38);
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("C", 3)==38);
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("D", 1)==31);
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("E", 3)==47);
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("F", 4)==52);
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("G", 5)==48);
         
-        //Sabemos que el costo de la licena clase "A" es de $20 y al mismo se le suman $8 de costo administrativo. 
+        
+        //Pruebas falsas
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("A", 1)!=20); 
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("B", 3)!=25); 
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("C", 4)!=35); 
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("D", 5)!=47); 
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("E", 4)!=44); 
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("F", 3)!=39); 
+        assertTrue(Calculos.CalcularCosto.CalcularCosto("G", 1)!=20); 
+
     }
     
 }
